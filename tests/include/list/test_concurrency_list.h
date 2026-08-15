efaa7b09022989b06834faf0f6d4fd342486c73d
Matteo Dubuisson <matteo.dubuisson@student.uclouvain.be>
2026-08-15 17:36:16 +0200

    Init concurency tests

---
+++ tests/include/list/test_concurrency_list.h
@@ -0,0 +1,8 @@
+#ifndef __TEST_CONCURRENCY_LIST__LIBRARY__
+#define __TEST_CONCURRENCY_LIST__LIBRARY__
+
+#include "list/list.h"
+
+int run_concurrency_tests(list_type_t type);
+
+#endif
---
+++ tests/list/test_concurrency_list.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <CUnit/Basic.h>
+#include <CUnit/CUnit.h>
+
+#include "list/test_concurrency_list.h"
+
